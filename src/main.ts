87828775619ab8f23cf22b2654a7da3ce162fee4
Erik <erkmgwr@gmail.com>
2025-04-06 18:31:58 -0300

    feat: add GitHub Actions workflow for unit testing and building on push to master; enhance CORS configuration in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConsoleLogger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new ConsoleLogger({
+      colors: true,
+      logLevels: ['error', 'warn', 'log', 'debug', 'verbose'],
+    }),
+  });
+
+  const allowedOrigins = [process.env.FRONTEND_URL];
 
   app.enableCors({
-    origin: (_, callback) => {
-      callback(null, true);
+    origin: (url, callback) => {
+      if (!url || allowedOrigins.includes(url as string)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
     },
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
